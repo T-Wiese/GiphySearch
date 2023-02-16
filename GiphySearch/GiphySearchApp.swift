7850a730ce60d1457d169f83b9f980f37a64bbf3
t-wiese <twiese@turntable.fm>
2023-02-16 10:53:20 -0800

    Initial Commit

---
+++ GiphySearch/GiphySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphySearchApp.swift
+//  GiphySearch
+//
+//  Created by Torin Wiese (Work) on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GiphySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
